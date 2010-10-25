f25493b7ebb9220a89afaa459c3e26fcf0da02dc
Rawane ZOSSOU <dev@raw1z.fr>
2010-10-25 17:35:33 +0200

    Add a rake task for updating the existing addressees mailboxes

--- lib/facteur.rb
+++ lib/facteur.rb
@@ -1,3 +1,14 @@
 require File.join(File.dirname(__FILE__), 'facteur/message_model')
 require File.join(File.dirname(__FILE__), 'facteur/mailbox_model')
 require File.join(File.dirname(__FILE__), 'facteur/addressee_model')
+require 'rails'
+
+module Facteur
+  module Rails
+    class Railtie < ::Rails::Railtie
+      rake_tasks do
+        load "facteur/rails/tasks/facteur.rake"
+      end
+    end
+  end
+end
