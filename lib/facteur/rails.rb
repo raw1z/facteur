d913d171b105447ca6b64b5aa4f18e30df2027f0
Rawane ZOSSOU <dev@raw1z.fr>
2011-02-20 22:57:12 +0100

    removed the rails tasks

--- lib/facteur/rails.rb
+++
@@ -1,9 +0,0 @@
-module Facteur
-  module Rails
-    class Railtie < ::Rails::Railtie
-      rake_tasks do
-        load File.dirname(__FILE__) + "facteur/rails/tasks/facteur.rake"
-      end
-    end
-  end
-end
