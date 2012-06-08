41fcc05cd15f2db95e35121af739787994071ab9
rexpraseo <rexpraseo@gmail.com>
2012-06-08 11:26:21 -0300

    Update master

--- lib/facteur/mailbox.rb
+++ lib/facteur/mailbox.rb
@@ -2,6 +2,8 @@ module Facteur
   if Object.const_defined? :ActiveRecord
     class Mailbox < ActiveRecord::Base
       include Facteur::MailboxModel
+      attr_accessible :name # stops ActiveRecord mass_assignment issues;
+                            #it appears this is the only attr that is directly set when a message is sent
     end
     
   elsif Object.const_defined? :Mongoid
